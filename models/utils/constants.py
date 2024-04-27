cc299dafab9f6cc3aa1b982d46e1fd5c04142938
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-27 03:40:38 -0400

    removed modules

--- models/utils/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DB_HOST = os.getenv("DB_HOST", "localhost") + ":8000"
-OLLAMA_HOST = os.getenv("OLLAMA_HOST", "localhost") + ":11434"
-SCRAPER_HOST = os.getenv("SCRAPER_HOST", "localhost") + ":8050"
-RECOMMENDER_HOST = os.getenv("RECOMMENDER_HOST", "localhost") + ":8030"
-LLM_HOST = os.getenv("LLM_HOST", "localhost") + ":8040"
